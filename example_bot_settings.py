e08b5acd7c48d9cdd0a8b178f0879b2a63fd4aa0
Pum <michael.duengler@gmail.com>
2018-07-11 15:42:04 +0200

    Create example_bot_settings.py

---
+++ example_bot_settings.py
@@ -0,0 +1,8 @@
+TOKEN = "MjM4NDk0NzU2NTIxMzc3Nzky.CunGFQ.wUILz7z6HoJzVeq6pyHPmVgQgV4"  # just paste the bot token in here, this token will not work
+prefix = [".", "plz "]  # multiple prefixes are possible
+default_game = ["testing things", "use .help", "love bot 9000"]  # can be anything, just follow the format
+openweathermap_key = "3800627bfd714728df4725f27b61ec33"   #open weathermap key, this key will not work
+weather_unit = "metric"
+daily_amount = 1000    # can be any integer
+daily_time = 18000       # in hours
+currency_name = "dolla"   # can be anything
